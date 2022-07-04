a84368ed5e628902dec5e293deae84c1ce3bd344
André Gabry <andre.gabry@icloud.com>
2022-07-03 22:02:13 -0300

    Start Project

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Money Care</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <div class="brand-flex">
+                <img src="assets/img/logo.svg">
+                <div class="brand-icons">
+                    <span class="material-icons-outlined">notifications</span>
+                    <span class="material-icons-outlined">account_circle</span>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar-main">
+            <div class="sidebar-user">
+                <img src="assets/img/profile.JPG">
+                <div>
+                    <h3>André Gabry</h3>
+                    <span>andre.gabry@icloud.com</span>
+                </div>
+            </div>
+            <div class="sidebar-menu">
+                <div class="menu-head">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
