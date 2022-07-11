6bd54a438dc9d35950352c128aa944ff694f3476
andre.gabry <andre.gabry@icloud.com>
2022-07-11 18:14:01 -0300

    Start Project

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,4 +3,8 @@ $(function (){
         $(".app_sidebar").toggleClass("app_sidebar_mobile");
         $(".app_main").toggleClass("app_main_mobile");
     });
+
+    $(".app_sidebar").parent().click(function (){
+
+    });
 });
