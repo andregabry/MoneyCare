366d7ccca9a32efb249d8cf69c33d0c1a02e24db
andre.gabry <andre.gabry@icloud.com>
2022-07-18 13:45:52 -0300

    Start Project

--- views/wallets.php
+++ views/wallets.php
@@ -1,19 +1,26 @@
 <section class="app_wallets row">
-    <article class="wallet_create widget_small">
-        <h2 class="fas fa-exclamation-circle"></h2>
-        <h3>Crie e gerencie suas carteiras</h3>
-        <p>Organize suas contas de diferentes fontes como <b>Minha Casa</b> para PF, <b>Minha Empresa</b> para PJ, ou
-            ainda <b>Cartão 6431</b> para organizar cartões. Controle tudo...</p>
-        <a href="modals.php #wallet_add" class="create_wallet"><i class="fas fa-plus-circle"></i> Adicionar Carteira</a>
-
-    </article>
-    <article class="widget_small" id="new_wallet">
-        <i class="fas fa-times-circle"></i>
-        <h2 class="fas fa-wallet"></h2>
-        <input data-walletedit="" type="text" name="wallet" value="SANTANDER">
-        <p class="wallet_balance">R$ 1.400,00</p>
-        <p class="wallet_income">Receitas: R$ 5.000,00</p>
-        <p class="wallet_expense">Despesas: R$ 3.600,00</p>
-    </article>
-
+    <div class="column">
+        <div class="row">
+            <article class="wallet_create gradient-purple gradient-hover gradient widget_small">
+                <h2 class="fas fa-exclamation-circle"></h2>
+                <h3>Crie e gerencie suas carteiras</h3>
+                <p>Organize suas contas de diferentes fontes como <b>Minha Casa</b> para PF, <b>Minha Empresa</b> para
+                    PJ, ou ainda <b>Cartão 6431</b> para organizar cartões. Controle tudo...</p>
+                <a href="modals.php #wallet_add" class="create_wallet"><i class="fas fa-plus-circle"></i> Adicionar
+                    Carteira</a>
+            </article>
+        </div>
+    </div>
+    <div class="column">
+        <div class="row">
+            <article class="wallet widget_small gradient gradient-blue gradient-hover" id="new_wallet">
+                <i class="fas fa-times-circle"></i>
+                <h2 class="fas fa-wallet"></h2>
+                <input data-walletedit="" type="text" name="wallet" value="SANTANDER">
+                <h3 class="wallet_balance">R$ 1.400,00</h3>
+                <p class="wallet_income">Receitas: R$ 5.000,00</p>
+                <p class="wallet_expense">Despesas: R$ 3.600,00</p>
+            </article>
+        </div>
+    </div>
 </section>
